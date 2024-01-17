3a5dc56b19aac0e8ef2ac0e7f04bfe01cffae5e0
Michael <michael.persikov@gmail.com>
2024-01-17 09:43:18 +0200

    delete: cash actions

--- src/components/ReduxChild.jsx
+++
@@ -1,9 +0,0 @@
-import { useSelector } from 'react-redux';
-
-export const ReduxChild = () => {
-    const cash = useSelector(state => state.cash.cash);
-
-    return (
-        <h2>{cash}</h2>
-    )
-}
